e0bf9d1bd69dcfa98eeb9ee741601f636505f536
Ankita-saha2000 <59726959+Ankita-saha2000@users.noreply.github.com>
2020-04-17 15:14:47 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <?php include 'includes/head.php';?>
+     <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+</head>
+<body>
+	
+<?php include 'includes/navbar.php';?>
+<div class="signup-form">
+            <img src="img/pic.png">
+            <form action="index.html" method="POST">
+                <input type ="text" placeholder="User Name" class="txt">
+                <input type ="email" placeholder="Email" class="txt">
+                <input type ="password" placeholder="Password" class="txt">
+                <input type ="button" value="User Name" class="btn">
+                <a href="#">Already Have a Account</a>
+            </form>
+        </div>
+<?php include 'includes/scripts.php';?>
+</body>
+</html>
+
+
+<!--- Check out my courses! -->
+<!-- <div class="udemy-course" style="position: fixed; bottom: 0; right: 0; margin-bottom: -5px; z-index: 100;">
+	<a href="https://www.google.com/search?q=shinchan&rlz=1C1CHBF_enIN859IN859&oq=sh&aqs=chrome.0.69i59j69i57j69i59j0l3j69i60j69i61.1407j0j7&sourceid=chrome&ie=UTF-8" target="_blank" style="z-index: 999!important; cursor: pointer!important;"><img src="img/sh.jpg" style="max-width: 100%; min-width: 100%;"></a>
+</div> -->
