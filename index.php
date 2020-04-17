f728e7d08881346561c4c4e221d61efa264f7bf6
Ankita-saha2000 <59726959+Ankita-saha2000@users.noreply.github.com>
2020-04-17 15:54:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<div class="carousel-inner">
 			<div class="carousel-item active"><img src="img/back.png"></div>
 			<div class="carousel-item"><img src="img/b2.jpg"></div>
-			<div class="carousel-item"><img src="img/bridge.png"></div>
+			
 		</div>
 	</div>
 	<!--- End Image Slider -->
@@ -20,8 +20,8 @@
 	<div class="container">
 		<div class="row justify-content-center text-center">
 			<div class="col-10 py-5">
-				<h2>Complete Bootstrap Course</h2>
-				<p class="lead">Bootstrap is an open-source front-end library with HTML and CSS based designs. This website is built with HTML5, CSS3 and Bootstrap 4.</p><a class="btn btn-purple btn-lg" href="https://w3newbie.com/courses/" target="_blank">Complete Bootstrap Course</a>
+				<h2>Job Hunt</h2>
+				<p class="lead">Contacting as many people as possible is a highly effective way to find a job. It is estimated that 50% or higher of all jobs are found through networking.</p><a class="btn btn-purple btn-lg" href="http://new.local/content.php" target="_blank">Study Material</a>
 			</div>
 		</div>
 	</div>
@@ -30,27 +30,27 @@
 	<!--- Start Jumbotron -->
 	<div class="jumbotron">
 		<div class="container">
-			<h2 class="text-center pt-5 pb-3">BUILT WITH THE BEST</h2>
+			<h2 class="text-center pt-5 pb-3">The best</h2>
 			<div class="row justify-content-center text-center">
 				<div class="col-10 col-md-4">
 					<div class="feature">
-						<img src="img/html5.png">
-						<h3>HTML5</h3>
-						<p>HTML5 is the fifth and current major version of the HTML standard, and subsumes XHTML.</p>
+						<img src="img/">
+						<h3>H</h3>
+						<p></p>
 					</div>
 				</div>
 				<div class="col-10 col-md-4">
 					<div class="feature">
-						<img src="img/bootstrap4.png">
-						<h3>BOOTSTRAP 4</h3>
-						<p>Bootstrap is an open-source front-end library with HTML and CSS based designs.</p>
+						<img src="img/">
+						<h3>CV</h3>
+						<p></p>
 					</div>
 				</div>
 				<div class="col-10 col-md-4">
 					<div class="feature">
-						<img src="img/css3.png">
-						<h3>CSS3</h3>
-						<p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.</p>
+						<img src="img/">
+						<h3>Study</h3>
+						<p></p>
 					</div>
 				</div>
 			</div>
@@ -64,9 +64,14 @@
 
 		<div class="row justify-content-center py-5">
 			<div class="col-lg-6">
-				<h3 class="pb-4">Learn to build Bootstrap websites</h3>
-				<p>The columns will automatically stack on top of each other when the screen is less than 992px wide.</p>
-				<p>Resize the browser window to see the effect. Responsive web design has become more important as the amount of mobile traffic now accounts for more than half of total internet traffic.</p><a class="btn btn-purple btn-lg" href="#">Bootstrap Course</a>
+				<h3 class="pb-4">Learn to build your CV</h3>
+				<p>Make sure you know when to use a CV.
+                   Pick the best CV format.
+                   Add your contact information the right way.
+                   Start with a CV personal profile (CV summary or CV objective)
+                   List your relevant work experience & key achievements.
+                   Build your CV education section correctly.</p>
+				<p>build the CV</p><a class="btn btn-purple btn-lg" href="#">CV</a>
 			</div>
 			<div class="col-lg-6"><img class="img-fluid" src="img/responsive.png"></div>
 		</div>
